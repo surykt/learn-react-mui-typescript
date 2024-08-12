838c8f9aa83e66866b86074382b1660a5310579b
Carlos Cardoso <puro_cros@hotmail.com>
2024-08-12 20:11:06 -0300

    feat: Add dark theme and toggle theme

---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,51 @@
+import {
+  createContext,
+  ReactElement,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
+import { Box, ThemeProvider } from "@mui/material";
+import { LightTheme, DarkTheme } from "../themes";
+
+interface IThemeContextData {
+  themeName: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () => {
+  return useContext(ThemeContext)
+}
+
+export const AppThemeProvider = ({ children }: any) => {
+  const [themeName, setThemeName] = useState<"light" | "dark">("light");
+
+  const toggleTheme = useCallback(() => {
+    setThemeName((oldThemeName) =>
+      oldThemeName === "light" ? "dark" : "light"
+    );
+  }, [themeName]);
+
+  const theme = useMemo(() => {
+    if (themeName === "light") return LightTheme;
+
+    return DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme}}>
+      <ThemeProvider theme={theme}>
+        <Box
+          width="100vw"
+          height="100vh"
+          bgcolor={theme.palette.background.default}
+        >
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
